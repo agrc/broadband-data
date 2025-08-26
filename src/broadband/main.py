8e6cafeddd4011a86861ee1a27b4a1cf81d5ae9b
Jake Adams <jdadams@utah.gov>
2025-08-25 18:04:47 -0600

    chore: use skid object design

--- src/broadband/main.py
+++ src/broadband/main.py
@@ -4,7 +4,6 @@
 Run the broadband-data skid as a cloud function.
 """
 
-import base64
 import json
 import logging
 import sys
@@ -14,8 +13,8 @@ from tempfile import TemporaryDirectory
 from types import SimpleNamespace
 
 import arcgis
-import functions_framework
-from cloudevents.http import CloudEvent
+import geopandas as gpd
+import pandas as pd
 from palletjack import extract, load, transform, utils
 from supervisor.message_handlers import SendGridHandler
 from supervisor.models import MessageDetails, Supervisor
@@ -29,137 +28,115 @@ except ImportError:
     import version
 
 
-def _get_secrets():
-    """A helper method for loading secrets from either a GCF mount point or the local src/skidname/secrets/secrets.json file
+class Skid:
+    def __init__(self):
+        self.secrets = SimpleNamespace(**self._get_secrets())
+        self.tempdir = TemporaryDirectory(ignore_cleanup_errors=True)
+        self.tempdir_path = Path(self.tempdir.name)
+        self.log_name = f"{config.LOG_FILE_NAME}_{datetime.now().strftime('%Y%m%d-%H%M%S')}.txt"
+        self.log_path = self.tempdir_path / self.log_name
+        self._initialize_supervisor()
+        self.skid_logger = logging.getLogger(config.SKID_NAME)
 
-    Raises:
-        FileNotFoundError: If the secrets file can't be found.
+    def __del__(self):
+        self.tempdir.cleanup()
 
-    Returns:
-        dict: The secrets .json loaded as a dictionary
-    """
+    @staticmethod
+    def _get_secrets():
+        """A helper method for loading secrets from either a GCF mount point or the local src/skidname/secrets/secrets.json file
 
-    secret_folder = Path("/secrets")
+        Raises:
+            FileNotFoundError: If the secrets file can't be found.
 
-    #: Try to get the secrets from the Cloud Function mount point
-    if secret_folder.exists():
-        return json.loads(Path("/secrets/app/secrets.json").read_text(encoding="utf-8"))
+        Returns:
+            dict: The secrets .json loaded as a dictionary
+        """
 
-    #: Otherwise, try to load a local copy for local development
-    secret_folder = Path(__file__).parent / "secrets"
-    if secret_folder.exists():
-        return json.loads((secret_folder / "secrets.json").read_text(encoding="utf-8"))
+        secret_folder = Path("/secrets")
 
-    raise FileNotFoundError("Secrets folder not found; secrets not loaded.")
+        #: Try to get the secrets from the Cloud Function mount point
+        if secret_folder.exists():
+            return json.loads(Path("/secrets/app/secrets.json").read_text(encoding="utf-8"))
 
+        #: Otherwise, try to load a local copy for local development
+        secret_folder = Path(__file__).parent / "secrets"
+        if secret_folder.exists():
+            return json.loads((secret_folder / "secrets.json").read_text(encoding="utf-8"))
 
-def _initialize(log_path, sendgrid_api_key):
-    """A helper method to set up logging and supervisor
+        raise FileNotFoundError("Secrets folder not found; secrets not loaded.")
 
-    Args:
-        log_path (Path): File path for the logfile to be written
-        sendgrid_api_key (str): The API key for sendgrid for this particular application
+    def _initialize_supervisor(self):
+        """A helper method to set up logging and supervisor
 
-    Returns:
-        Supervisor: The supervisor object used for sending messages
-    """
+        Args:
+            log_path (Path): File path for the logfile to be written
+            sendgrid_api_key (str): The API key for sendgrid for this particular application
 
-    skid_logger = logging.getLogger(config.SKID_NAME)
-    skid_logger.setLevel(config.LOG_LEVEL)
-    palletjack_logger = logging.getLogger("palletjack")
-    palletjack_logger.setLevel(config.LOG_LEVEL)
+        Returns:
+            Supervisor: The supervisor object used for sending messages
+        """
 
-    cli_handler = logging.StreamHandler(sys.stdout)
-    cli_handler.setLevel(config.LOG_LEVEL)
-    formatter = logging.Formatter(
-        fmt="%(levelname)-7s %(asctime)s %(name)15s:%(lineno)5s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-    )
-    cli_handler.setFormatter(formatter)
+        skid_logger = logging.getLogger(config.SKID_NAME)
+        skid_logger.setLevel(config.LOG_LEVEL)
+        palletjack_logger = logging.getLogger("palletjack")
+        palletjack_logger.setLevel(config.LOG_LEVEL)
 
-    log_handler = logging.FileHandler(log_path, mode="w")
-    log_handler.setLevel(config.LOG_LEVEL)
-    log_handler.setFormatter(formatter)
-
-    skid_logger.addHandler(cli_handler)
-    skid_logger.addHandler(log_handler)
-    palletjack_logger.addHandler(cli_handler)
-    palletjack_logger.addHandler(log_handler)
-
-    #: Log any warnings at logging.WARNING
-    #: Put after everything else to prevent creating a duplicate, default formatter
-    #: (all log messages were duplicated if put at beginning)
-    logging.captureWarnings(True)
-
-    skid_logger.debug("Creating Supervisor object")
-    skid_supervisor = Supervisor(handle_errors=False)
-    sendgrid_settings = config.SENDGRID_SETTINGS
-    sendgrid_settings["api_key"] = sendgrid_api_key
-    skid_supervisor.add_message_handler(
-        SendGridHandler(
-            sendgrid_settings=sendgrid_settings, client_name=config.SKID_NAME, client_version=version.__version__
+        cli_handler = logging.StreamHandler(sys.stdout)
+        cli_handler.setLevel(config.LOG_LEVEL)
+        formatter = logging.Formatter(
+            fmt="%(levelname)-7s %(asctime)s %(name)15s:%(lineno)5s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
         )
-    )
+        cli_handler.setFormatter(formatter)
 
-    return skid_supervisor
+        log_handler = logging.FileHandler(self.log_path, mode="w")
+        log_handler.setLevel(config.LOG_LEVEL)
+        log_handler.setFormatter(formatter)
 
+        skid_logger.addHandler(cli_handler)
+        skid_logger.addHandler(log_handler)
+        palletjack_logger.addHandler(cli_handler)
+        palletjack_logger.addHandler(log_handler)
 
-def _remove_log_file_handlers(log_name, loggers):
-    """A helper function to remove the file handlers so the tempdir will close correctly
+        #: Log any warnings at logging.WARNING
+        #: Put after everything else to prevent creating a duplicate, default formatter
+        #: (all log messages were duplicated if put at beginning)
+        logging.captureWarnings(True)
 
-    Args:
-        log_name (str): The logfiles filename
-        loggers (List<str>): The loggers that are writing to log_name
-    """
+        skid_logger.debug("Creating Supervisor object")
+        self.supervisor = Supervisor(handle_errors=False)
+        sendgrid_settings = config.SENDGRID_SETTINGS
+        sendgrid_settings["api_key"] = self.secrets.SENDGRID_API_KEY
+        self.supervisor.add_message_handler(
+            SendGridHandler(
+                sendgrid_settings=sendgrid_settings, client_name=config.SKID_NAME, client_version=version.__version__
+            )
+        )
 
-    for logger in loggers:
-        for handler in logger.handlers:
-            try:
-                if log_name in handler.stream.name:
-                    logger.removeHandler(handler)
-                    handler.close()
-            except Exception:
-                pass
+    def _remove_log_file_handlers(self, log_name, loggers):
+        """A helper function to remove the file handlers so the tempdir will close correctly
 
+        Args:
+            log_name (str): The logfiles filename
+            loggers (List<str>): The loggers that are writing to log_name
+        """
 
-def process():
-    """The main function that does all the work."""
+        for logger in loggers:
+            for handler in logger.handlers:
+                try:
+                    if log_name in handler.stream.name:
+                        logger.removeHandler(handler)
+                        handler.close()
+                except Exception:
+                    pass
 
-    #: Set up secrets, tempdir, supervisor, and logging
-    start = datetime.now()
+    def process(self):
+        """The main function that does all the work."""
 
-    secrets = SimpleNamespace(**_get_secrets())
-
-    with TemporaryDirectory() as tempdir:
-        tempdir_path = Path(tempdir)
-        log_name = f"{config.LOG_FILE_NAME}_{start.strftime('%Y%m%d-%H%M%S')}.txt"
-        log_path = tempdir_path / log_name
-
-        skid_supervisor = _initialize(log_path, secrets.SENDGRID_API_KEY)
-        module_logger = logging.getLogger(config.SKID_NAME)
+        #: Set up secrets, tempdir, supervisor, and logging
+        start = datetime.now()
 
         #: Get our GIS object via the ArcGIS API for Python
-        gis = arcgis.gis.GIS(config.AGOL_ORG, secrets.AGOL_USER, secrets.AGOL_PASSWORD)
-
-        #########################################################################
-        #: Use the various palletjack classes and other code to do your work here
-        #########################################################################
-
-        module_logger.info("Log messages with module_logger.info() or module_logger.debug()")
-
-        #: Create a extract object to load your new data
-        extractor = extract.PostgresLoader("host", "database", "user", "password")
-        new_data_df = extractor.read_table_into_dataframe("table_name", "index_column", "crs", "shape_column")
-
-        #: Transform your data
-        new_data_df = new_data_df["new_column"] = "do custom transform stuff here"
-        new_data_df = transform.DataCleaning.rename_dataframe_columns_for_agol(new_data_df)
-
-        #: Use retry for operations that may fail randomly (network issues, etc)
-        utils.retry("method_to_retry", "arg1", keyword_arg="arg2")
-
-        #: Create a load object to load your new data
-        loader = load.FeatureServiceUpdater(gis, "item_id")
-        loader.update_features(new_data_df)
+        self.gis = arcgis.GIS(config.AGOL_ORG, self.secrets.AGOL_USER, self.secrets.AGOL_PASSWORD)
 
         end = datetime.now()
 
@@ -177,41 +154,21 @@ def process():
         ]
 
         summary_message.message = "\n".join(summary_rows)
-        summary_message.attachments = tempdir_path / log_name
+        summary_message.attachments = self.tempdir_path / self.log_name
 
-        skid_supervisor.notify(summary_message)
+        self.supervisor.notify(summary_message)
 
         #: Remove file handler so the tempdir will close properly
         loggers = [logging.getLogger(config.SKID_NAME), logging.getLogger("palletjack")]
-        _remove_log_file_handlers(log_name, loggers)
+        self._remove_log_file_handlers(log_name, loggers)  #: FIXME
 
 
-@functions_framework.cloud_event
-def subscribe(cloud_event: CloudEvent) -> None:
-    """Entry point for Google Cloud Function triggered by pub/sub event
-
-    Args:
-         cloud_event (CloudEvent):  The CloudEvent object with data specific to this type of
-                        event. The `type` field maps to
-                         `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
-                        The `data` field maps to the PubsubMessage data
-                        in a base64-encoded string. The `attributes` field maps
-                        to the PubsubMessage attributes if any is present.
-    Returns:
-        None. The output is written to Cloud Logging.
-    """
-
-    #: This function must be called 'subscribe' to act as the Google Cloud Function entry point. It must accept the
-    #: CloudEvent object as the only argument.
-
-    #: You can get the message-body value from the Cloud Scheduler event sent via pub/sub to customize the run
-    if base64.b64decode(cloud_event.data["message"]["data"]).decode() == "foo":
-        pass
-
-    #: Call process() and any other functions you want to be run as part of the skid here.
-    process()
+def entry():
+    skid = Skid()
+    skid.process()
 
 
 #: Putting this here means you can call the file via `python main.py` and it will run. Useful for pre-GCF testing.
 if __name__ == "__main__":
-    process()
+    skid = Skid()
+    skid.process()
