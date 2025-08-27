78ea0334fd00551d3ac95dc51051684ecabd27d0
Jake Adams <jdadams@utah.gov>
2025-08-27 17:54:42 -0600

    feat: first-pass reproduction of notebook

--- src/broadband/config.py
+++ src/broadband/config.py
@@ -10,14 +10,18 @@ SKID_NAME = "broadband-data"
 AGOL_ORG = "https://utah.maps.arcgis.com"
 SENDGRID_SETTINGS = {  #: Settings for SendGridHandler
     "from_address": "noreply@utah.gov",
-    "to_addresses": "",
+    "to_addresses": "jdadams@utah.gov",
     "prefix": f"{SKID_NAME} on {socket.gethostname()}: ",
 }
 LOG_LEVEL = logging.DEBUG
 LOG_FILE_NAME = "log"
 
-FEATURE_LAYER_ITEMID = ""
-JOIN_COLUMN = ""
-ATTACHMENT_LINK_COLUMN = ""
-ATTACHMENT_PATH_COLUMN = ""
-FIELDS = {}
+#: Hexes from SGID on AGOL
+HEXES_LEVEL_6_ITEMID = "992228ada5cd4016b5f9c4d68751d1ca"
+HEXES_LEVEL_7_ITEMID = "3dcd072755cd453885bda7dc69a82fef"
+HEXES_LEVEL_8_ITEMID = "008865ed31514569a9fd11a6dc8a7ee0"
+
+SERVICE_HEXES_6_ITEMID = "57a781feba684fe4a8410bf1e8e61d4f"
+SERVICE_HEXES_7_ITEMID = "834f5cc5d22249f2a6dcf8636a9b8d1b"
+SERVICE_HEXES_8_ITEMID = "25c23ead840e492a97dac6e117df72d8"
+SERVICE_RECORDS_ITEMID = "a13eebdb3676417e94569b1483f0c6b5"
---
+++ src/broadband/utils.py
@@ -0,0 +1,127 @@
+import geopandas as gpd
+import h3.api.numpy_int as h3
+import numpy as np
+import pandas as pd
+from palletjack import utils as pjutils
+
+
+def create_service_polygons_at_hex_level(
+    service_records: pd.DataFrame, hex_level: int, hex_polygons: pd.DataFrame
+) -> gpd.GeoDataFrame:
+    """Creates polygons representing service by provider, speeds, and technology at a given H3 hex level.
+
+    Args:
+        service_records (pd.DataFrame): All the service records to condense
+        hex_level (int): Desired H3 hex level
+        hex_polygons (pd.DataFrame): A spatially-enabled dataframe of H3 polygons at the desired level
+
+    Returns:
+        gpd.GeoDataFrame: Representation of what service is available where by provider, speeds, and technology
+    """
+
+    hex_id_field = f"h3_res{hex_level}_id"
+    service_at_level = service_by_hex_level(service_records, hex_id_field, hex_polygons)
+    service_gdf = pjutils.convert_to_gdf(service_at_level)
+    service_dissolved = service_gdf.dissolve(
+        by=["common_tech", "brand_name", "max_advertised_download_speed", "max_advertised_upload_speed"]
+    )
+    service_dissolved = categorize_service(service_dissolved.reset_index())
+
+    return service_dissolved
+
+
+def classify_common_tech(service_data_df: pd.DataFrame) -> pd.DataFrame:
+    conditions = [
+        service_data_df["technology_name"] == "Cable",
+        service_data_df["technology_name"] == "Copper",
+        service_data_df["technology_name"] == "Fiber to the Premises",
+        service_data_df["technology_name"].isin(
+            ["LBR Fixed Wireless", "Licensed Fixed Wireless", "Unlicensed Fixed Wireless"]
+        ),
+        service_data_df["technology_name"].isin(["GSO Satellite", "NGSO Satellite"]),
+    ]
+    tech_choices = [
+        "Cable",
+        "DSL",
+        "Fiber",
+        "Fixed Wireless",
+        "Satellite",
+    ]
+
+    service_data_df["common_tech"] = np.select(conditions, tech_choices, "Other Tech")
+
+    return service_data_df
+
+
+def categorize_service(service_data_df):
+    conditions = [
+        service_data_df["common_tech"] == "Cable",
+        service_data_df["common_tech"] == "DSL",
+        service_data_df["common_tech"] == "Fiber",
+        service_data_df["common_tech"] == "Fixed Wireless",
+        service_data_df["common_tech"] == "Satellite",
+    ]
+
+    choices = ["wired", "wired", "wired", "wireless", "satellite"]
+
+    service_data_df["category"] = np.select(conditions, choices, "Other Category")
+
+    return service_data_df
+
+
+def h3_to_parent(h3_str: str, parent_level: int) -> str:
+    return h3.h3_to_string(h3.h3_to_parent(h3.string_to_h3(h3_str), parent_level))
+
+
+def service_by_hex_level(all_records: pd.DataFrame, hex_id_field: str, hexes_df: pd.DataFrame) -> pd.DataFrame:
+    #: Calc max up/down speeds per hex/tech/provider
+    residential_only = all_records[all_records["business_residential_code"].isin(["R", "X"])]
+    individual_records_down = residential_only.groupby([hex_id_field, "technology_name", "brand_name", "common_tech"])[
+        "max_advertised_download_speed"
+    ].max()
+    individual_records_up = residential_only.groupby([hex_id_field, "technology_name", "brand_name", "common_tech"])[
+        "max_advertised_upload_speed"
+    ].max()
+    individual_records = pd.concat([individual_records_down, individual_records_up], axis=1).reset_index()
+
+    #: Get the speeds as ints
+    individual_records["max_advertised_download_speed"] = individual_records["max_advertised_download_speed"].astype(
+        int
+    )
+    individual_records["max_advertised_upload_speed"] = individual_records["max_advertised_upload_speed"].astype(int)
+
+    #: Merge with hexes, only keeping hexes that have service
+    all_record_hexes_service = hexes_df.merge(individual_records, left_on="hex_id", right_on=hex_id_field, how="right")
+
+    return all_record_hexes_service
+
+
+def max_service_by_hex_all_providers(service_records: pd.DataFrame) -> pd.DataFrame:
+    res_only = service_records[service_records["business_residential_code"].isin(["R", "X"])]
+
+    maxes = (
+        res_only.groupby(["h3_res8_id", "brand_name", "common_tech", "category"])[
+            ["max_advertised_download_speed", "max_advertised_upload_speed"]
+        ]
+        .agg("max")
+        .reset_index()
+    )
+
+    #: Fix types for AGOL
+    field_map = {
+        "max_advertised_download_speed": "int16",
+        "max_advertised_upload_speed": "int16",
+    }
+    for field, dtype in field_map.items():
+        if field in maxes.columns:
+            maxes[field] = maxes[field].astype(dtype)
+
+    #: Clean up fields, provider names
+    maxes.drop(
+        columns=["frn", "provider_id", "location_id", "technology", "low_latency", "state_usps", "block_geoid"],
+        inplace=True,
+        errors="ignore",
+    )
+    maxes["brand_name"] = maxes["brand_name"].replace({"Utah Telecommunication Open Infrastructure Agency": "UTOPIA"})
+
+    return maxes
