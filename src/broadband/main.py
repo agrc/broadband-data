45e14c2f5e4207c82663840f5fe3284ee640ecd6
Jake Adams <jdadams@utah.gov>
2025-10-02 17:27:03 -0600

    chore: try one from stackexchange

--- src/broadband/main.py
+++ src/broadband/main.py
@@ -235,7 +235,7 @@ class Skid:
         base_headers = {
             "username": self.secrets.BDC_USERNAME,
             "hash_value": self.secrets.BDC_HASH,
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+            "User-Agent": "play/0.0.0",
         }
 
         bdc_session = requests.Session()
